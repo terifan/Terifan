a8beab9b8a5e9eedcb7ad56cfe050352304d2e52
terifan <pinjata69@gmail.com>
2015-03-03 16:45:51 +0100

    added drag and drop implementation

---
+++ src/org/terifan/ui/DragAndDrop.java
@@ -0,0 +1,257 @@
+package org.terifan.ui;
+
+import java.awt.Point;
+import java.awt.datatransfer.DataFlavor;
+import java.awt.datatransfer.Transferable;
+import java.awt.datatransfer.UnsupportedFlavorException;
+import java.awt.dnd.DnDConstants;
+import java.awt.dnd.DragGestureEvent;
+import java.awt.dnd.DragGestureListener;
+import java.awt.dnd.DragSource;
+import java.io.IOException;
+import java.io.Serializable;
+import javax.swing.JComponent;
+import javax.swing.TransferHandler;
+import javax.swing.TransferHandler.TransferSupport;
+
+
+/**
+ * Default Drag-n-Drop implementation that wraps/hides Oracle's garbage implementation.
+ *
+ * <code>
+ *	new DragAndDrop(component)
+ *	{
+ *		@Override
+ *		public Object drag(Point aDragOrigin)
+ *		{
+ *			return "hello world"; // a serializable object
+ *		}
+ *
+ *		@Override
+ *		public void drop(DropEvent aDropEvent)
+ *		{
+ *			System.out.println(aDropEvent.getTransferData());
+ *		}
+ *	};
+ * </code>
+ */
+public abstract class DragAndDrop
+{
+	private final static DataFlavor javaSerializedObjectMimeType = new DataFlavor(Serializable.class, "Java Serialized Object");
+
+	protected JComponent mComponent;
+
+
+	public DragAndDrop(JComponent aComponent)
+	{
+		mComponent = aComponent;
+
+		DragSource.getDefaultDragSource().createDefaultDragGestureRecognizer(mComponent, DnDConstants.ACTION_COPY, new MyDragGestureListener());
+
+		mComponent.setTransferHandler(new MyTransferHandler());
+	}
+
+
+	public JComponent getComponent()
+	{
+		return mComponent;
+	}
+
+
+	/**
+	 * Handles drags. This implementation does nothing and returns null.
+	 *
+	 * @param aDragOrigin
+	 *   the position on the component where the drag occurred.
+	 */
+	public Object drag(Point aDragOrigin)
+	{
+		return null;
+	}
+
+
+	/**
+	 * Handles drops. This implementation does nothing.
+	 *
+	 * @param aDropEvent
+	 *   an object containing details about the drop.
+	 */
+	public void drop(DropEvent aDropEvent)
+	{
+	}
+
+
+	private class MyDragGestureListener implements DragGestureListener
+	{
+		@Override
+		public void dragGestureRecognized(DragGestureEvent aDrag)
+		{
+			aDrag.startDrag(DragSource.DefaultCopyDrop, new MyTransferable(aDrag));
+		}
+	}
+
+
+	private class MyTransferHandler extends TransferHandler
+	{
+		@Override
+		public boolean canImport(TransferSupport aSupport)
+		{
+			return aSupport.isDataFlavorSupported(javaSerializedObjectMimeType);
+		}
+
+
+		@Override
+		public boolean importData(TransferSupport aSupport)
+		{
+			drop(new DropEvent(aSupport));
+			return true;
+		}
+
+
+		@Override
+		public int getSourceActions(JComponent aComponent)
+		{
+			return COPY_OR_MOVE;
+		}
+
+
+		@Override
+		protected Transferable createTransferable(JComponent aComponent)
+		{
+			return new MyTransferable(null);
+		}
+	};
+
+
+	private class MyTransferable implements Transferable
+	{
+		private Point mDragOrigin;
+
+
+		public MyTransferable(DragGestureEvent aEvent)
+		{
+			mDragOrigin = aEvent.getDragOrigin();
+		}
+
+
+		@Override
+		public DataFlavor[] getTransferDataFlavors()
+		{
+			return new DataFlavor[]{javaSerializedObjectMimeType};
+		}
+
+
+		@Override
+		public boolean isDataFlavorSupported(DataFlavor aFlavor)
+		{
+			return javaSerializedObjectMimeType.equals(aFlavor);
+		}
+
+
+		@Override
+		public Object getTransferData(DataFlavor aFlavor)
+		{
+			return drag(mDragOrigin);
+		}
+	}
+
+
+	public static class DropEvent
+	{
+		public final static int COPY = DnDConstants.ACTION_COPY;
+		public final static int MOVE = DnDConstants.ACTION_MOVE;
+
+		private int mDropAction;
+		private Point mDropLocation;
+		private Object mTransferData;
+
+
+		public DropEvent(TransferSupport aSupport)
+		{
+			try
+			{
+				mTransferData = aSupport.getTransferable().getTransferData(javaSerializedObjectMimeType);
+			}
+			catch (UnsupportedFlavorException | IOException e)
+			{
+				throw new IllegalStateException(e);
+			}
+			mDropLocation = aSupport.getDropLocation().getDropPoint();
+			mDropAction = aSupport.getDropAction();
+		}
+
+
+		public Point getDropLocation()
+		{
+			return mDropLocation;
+		}
+
+
+		public int getDropAction()
+		{
+			return mDropAction;
+		}
+
+
+		public Object getTransferData()
+		{
+			return mTransferData;
+		}
+
+
+		@Override
+		public String toString()
+		{
+			String action = mDropAction == MOVE ? "move" : mDropAction == COPY ? "copy" : "other";
+			String at = "[" + mDropLocation.x + "," + mDropLocation.y + "]";
+			return "{action=" + action + ", dropLocation=" + at + ", transferable=" + getTransferData() + "}";
+		}
+	}
+
+
+//	public static void main(String ... args)
+//	{
+//		try
+//		{
+//			JTree tree = new JTree();
+//			JPanel panel = new JPanel(null);
+//
+//			new DragAndDrop(tree)
+//			{
+//				@Override
+//				public Object drag(Point aDragOrigin)
+//				{
+//					return tree.getClosestPathForLocation(aDragOrigin.x, aDragOrigin.y).getLastPathComponent().toString();
+//				}
+//			};
+//
+//			new DragAndDrop(panel)
+//			{
+//				@Override
+//				public void drop(DropEvent aDropEvent)
+//				{
+//					JLabel label = new JLabel("" + aDropEvent.getTransferData());
+//					label.setLocation(aDropEvent.getDropLocation());
+//					label.setSize(100,20);
+//					panel.add(label);
+//					panel.repaint();
+//				}
+//			};
+//
+//			JPanel pane = new JPanel(new GridLayout(1,2));
+//			pane.add(tree);
+//			pane.add(panel);
+//
+//			JFrame frame = new JFrame();
+//			frame.add(pane);
+//			frame.setSize(1024, 768);
+//			frame.setLocationRelativeTo(null);
+//			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//			frame.setVisible(true);
+//		}
+//		catch (Throwable e)
+//		{
+//			e.printStackTrace(System.out);
+//		}
+//	}
+}
---
+++ src/org/terifan/ui/NullLayoutManager.java
@@ -0,0 +1,54 @@
+package org.terifan.ui;
+
+import java.awt.Component;
+import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.LayoutManager;
+import java.awt.Rectangle;
+
+
+public class NullLayoutManager implements LayoutManager
+{
+	@Override
+	public void addLayoutComponent(String aName, Component aComp)
+	{
+	}
+
+
+	@Override
+	public void removeLayoutComponent(Component aComp)
+	{
+	}
+
+
+	@Override
+	public void layoutContainer(Container aParent)
+	{
+	}
+
+
+	@Override
+	public Dimension minimumLayoutSize(Container aParent)
+	{
+		return new Dimension(0,0);
+	}
+
+
+	@Override
+	public Dimension preferredLayoutSize(Container aParent)
+	{
+		if (aParent.getComponentCount() == 0)
+		{
+			return new Dimension();
+		}
+
+		Rectangle bounds = aParent.getComponent(0).getBounds();
+
+		for (int i = 1; i < aParent.getComponentCount(); i++)
+		{
+			bounds.add(aParent.getComponent(i).getBounds());
+		}
+
+		return new Dimension(bounds.x + bounds.width, bounds.y + bounds.height);
+	}
+}
