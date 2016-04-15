b426aecc7b028e9935ea8a0ca7a194aac24b4fa9
Terifan <pinjata69@gmail.com>
2016-04-16 00:06:49 +0200

    updated

--- src/org/terifan/util/Tuple.java
+++ src/org/terifan/util/Tuple.java
@@ -1,6 +1,7 @@
 package org.terifan.util;
 
 import java.io.Serializable;
+import java.util.Comparator;
 import java.util.Objects;
 
 
@@ -15,6 +16,11 @@ import java.util.Objects;
  */
 public class Tuple<T, U> implements Serializable, Comparable<Tuple<T, U>>
 {
+	private final static long serialVersionUID = 1L;
+
+	private final Comparator<T> mTComparator = (T aO1, T aO2) -> ((Comparable<T>)aO1).compareTo(aO2);
+	private final Comparator<U> mUComparator = (U aO1, U aO2) -> ((Comparable<U>)aO1).compareTo(aO2);
+
 	private T mFirst;
 	private U mSecond;
 
@@ -57,33 +63,26 @@ public class Tuple<T, U> implements Serializable, Comparable<Tuple<T, U>>
 	@Override
 	public boolean equals(Object aObject)
 	{
+		if (aObject == this)
+		{
+			return true;
+		}
+		
 		if (aObject instanceof Tuple)
 		{
 			Tuple<T, U> t = (Tuple<T, U>)aObject;
 
 			T f1 = mFirst;
 			T f2 = t.mFirst;
-
-			if (f1 == null && f2 == null)
-			{
-			}
-			else if (f1 == null && f2 != null || f1 != null && f2 == null || !f1.equals(f2))
-			{
-				return false;
-			}
-
 			U s1 = mSecond;
 			U s2 = t.mSecond;
 
-			if (s1 == null && s2 == null)
+			if (f1 == f2 && s1 == s2)
 			{
-			}
-			else if (s1 == null && s2 != null || s1 != null && s2 == null || !s1.equals(s2))
-			{
-				return false;
+				return true;
 			}
 
-			return true;
+			return f1 != null && s1 != null && f1.equals(f2) && s1.equals(s2);
 		}
 
 		return false;
@@ -163,45 +162,54 @@ public class Tuple<T, U> implements Serializable, Comparable<Tuple<T, U>>
 	@Override
 	public int compareTo(Tuple<T, U> aTuple)
 	{
-		int r;
-
-		if (getFirst() == null && aTuple.getFirst() == null)
+		int i = Objects.compare(mFirst, aTuple.mFirst, mTComparator);
+		
+		if (i == 0)
 		{
-			r = 0;
-		}
-		else if (getFirst() == null && aTuple.getFirst() != null)
-		{
-			r = -1;
-		}
-		else if (getFirst() != null && aTuple.getFirst() == null)
-		{
-			r = 1;
-		}
-		else
-		{
-			r = ((Comparable<T>)getFirst()).compareTo(aTuple.getFirst());
+			return Objects.compare(mSecond, aTuple.mSecond, mUComparator);
 		}
 
-		if (r == 0)
-		{
-			if (getSecond() == null && aTuple.getSecond() == null)
-			{
-				r = 0;
-			}
-			else if (getSecond() == null && aTuple.getSecond() != null)
-			{
-				r = -1;
-			}
-			else if (getSecond() != null && aTuple.getSecond() == null)
-			{
-				r = 1;
-			}
-			else
-			{
-				r = ((Comparable<U>)getSecond()).compareTo(aTuple.getSecond());
-			}
-		}
+		return i;
 
-		return r;
+//		int r;
+//
+//		if (getFirst() == null && aTuple.getFirst() == null)
+//		{
+//			r = 0;
+//		}
+//		else if (getFirst() == null && aTuple.getFirst() != null)
+//		{
+//			r = -1;
+//		}
+//		else if (getFirst() != null && aTuple.getFirst() == null)
+//		{
+//			r = 1;
+//		}
+//		else
+//		{
+//			r = ((Comparable<T>)getFirst()).compareTo(aTuple.getFirst());
+//		}
+//
+//		if (r == 0)
+//		{
+//			if (getSecond() == null && aTuple.getSecond() == null)
+//			{
+//				r = 0;
+//			}
+//			else if (getSecond() == null && aTuple.getSecond() != null)
+//			{
+//				r = -1;
+//			}
+//			else if (getSecond() != null && aTuple.getSecond() == null)
+//			{
+//				r = 1;
+//			}
+//			else
+//			{
+//				r = ((Comparable<U>)getSecond()).compareTo(aTuple.getSecond());
+//			}
+//		}
+//
+//		return r;
 	}
 }
