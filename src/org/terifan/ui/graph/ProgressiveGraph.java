f56da63c1c24546607fc12a8c7870ce7c408dd00
terifan <pinjata69@gmail.com>
2017-07-17 22:40:41 +0200

    update

--- src/org/terifan/ui/graph/ProgressiveGraph.java
+++ src/org/terifan/ui/graph/ProgressiveGraph.java
@@ -2,6 +2,7 @@ package org.terifan.ui.graph;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
@@ -28,15 +29,17 @@ public class ProgressiveGraph extends JComponent
 	private String mTitle;
 	private String mUnit;
 
-	private Color mLineColor = new Color(17, 125, 187);
-	private Color mFillColor = new Color(17, 125, 187, 32);
-	private Color mGridColor = new Color(241, 246, 250);
-	private Color mAverageLineColor = new Color(50,255,80, 128);
-	private int mBottomLegendHeight = 16;
-	private int mRightLegendWidth = 40;
-	private int mTitleLegendHeight = 24;
-	private int mTimeSpacing = 60;
-	private int mGridSize = 8;
+	private Font mFont;
+	private Color mLineColor;
+	private Color mFillColor;
+	private Color mGridColor;
+	private Color mBorderColor;
+	private Color mAverageLineColor;
+	private int mBottomLegendHeight;
+	private int mRightLegendWidth;
+	private int mTitleLegendHeight;
+	private int mTimeSpacing;
+	private int mGridSize;
 	private boolean mDrawAverage;
 
 
@@ -48,7 +51,199 @@ public class ProgressiveGraph extends JComponent
 		mValues = new long[aSequenceLength];
 		mTimes = new long[aSequenceLength];
 
-		setBackground(Color.WHITE);
+		mGridSize = 8;
+		mTimeSpacing = 60;
+		mTitleLegendHeight = 20;
+		mRightLegendWidth = 40;
+		mBottomLegendHeight = 20;
+		mAverageLineColor = new Color(50,255,80, 128);
+		mBorderColor = new Color(241, 246, 250);
+		mGridColor = new Color(241, 246, 250);
+		mFillColor = new Color(17, 125, 187, 32);
+		mLineColor = new Color(17, 125, 187);
+		mFont = new Font("segoe ui", Font.PLAIN, 12);
+
+		setGridColor(new Color(221, 226, 230));
+		setBackground(new Color(240, 240, 240));
+	}
+	
+	
+	public void scaleUI(double aScale)
+	{
+		mGridSize *= aScale;
+		mTimeSpacing *= aScale;
+		mTitleLegendHeight *= aScale;
+		mRightLegendWidth *= aScale;
+		mBottomLegendHeight *= aScale;
+		mFont = mFont.deriveFont((float)(mFont.getSize() * aScale));
+	}
+
+
+	public Color getBorderColor()
+	{
+		return mBorderColor;
+	}
+
+
+	public void setBorderColor(Color aBorderColor)
+	{
+		mBorderColor = aBorderColor;
+	}
+
+
+	public long getCumulativeSum()
+	{
+		return mCumulativeSum;
+	}
+
+
+	public void setCumulativeSum(long aCumulativeSum)
+	{
+		mCumulativeSum = aCumulativeSum;
+	}
+
+
+	public long getMaxValue()
+	{
+		return mMaxValue;
+	}
+
+
+	public void setMaxValue(long aMaxValue)
+	{
+		mMaxValue = aMaxValue;
+	}
+
+
+	public int getCounter()
+	{
+		return mCounter;
+	}
+
+
+	public void setCounter(int aCounter)
+	{
+		mCounter = aCounter;
+	}
+
+
+	public String getTitle()
+	{
+		return mTitle;
+	}
+
+
+	public void setTitle(String aTitle)
+	{
+		mTitle = aTitle;
+	}
+
+
+	public String getUnit()
+	{
+		return mUnit;
+	}
+
+
+	public void setUnit(String aUnit)
+	{
+		mUnit = aUnit;
+	}
+
+
+	public Color getAverageLineColor()
+	{
+		return mAverageLineColor;
+	}
+
+
+	public void setAverageLineColor(Color aAverageLineColor)
+	{
+		mAverageLineColor = aAverageLineColor;
+	}
+
+
+	public int getBottomLegendHeight()
+	{
+		return mBottomLegendHeight;
+	}
+
+
+	public void setBottomLegendHeight(int aBottomLegendHeight)
+	{
+		mBottomLegendHeight = aBottomLegendHeight;
+	}
+
+
+	public Color getLineColor()
+	{
+		return mLineColor;
+	}
+
+
+	public void setLineColor(Color aLineColor)
+	{
+		mLineColor = aLineColor;
+	}
+
+
+	public Color getFillColor()
+	{
+		return mFillColor;
+	}
+
+
+	public void setFillColor(Color aFillColor)
+	{
+		mFillColor = aFillColor;
+	}
+
+
+	public int getRightLegendWidth()
+	{
+		return mRightLegendWidth;
+	}
+
+
+	public void setRightLegendWidth(int aRightLegendWidth)
+	{
+		mRightLegendWidth = aRightLegendWidth;
+	}
+
+
+	public int getTitleLegendHeight()
+	{
+		return mTitleLegendHeight;
+	}
+
+
+	public void setTitleLegendHeight(int aTitleLegendHeight)
+	{
+		mTitleLegendHeight = aTitleLegendHeight;
+	}
+
+
+	public int getTimeSpacing()
+	{
+		return mTimeSpacing;
+	}
+
+
+	public void setTimeSpacing(int aTimeSpacing)
+	{
+		mTimeSpacing = aTimeSpacing;
+	}
+
+
+	public int getGridSize()
+	{
+		return mGridSize;
+	}
+
+
+	public void setGridSize(int aGridSize)
+	{
+		mGridSize = aGridSize;
 	}
 
 
@@ -60,7 +255,7 @@ public class ProgressiveGraph extends JComponent
 
 	public void setGridColor(Color aGridColor)
 	{
-		this.mGridColor = aGridColor;
+		mGridColor = aGridColor;
 	}
 
 
@@ -77,31 +272,46 @@ public class ProgressiveGraph extends JComponent
 		return this;
 	}
 
+	private int mCounter2;
+	private long mTemp;
 
 	public synchronized void add(long aValue)
 	{
-		if (mConstantMaxValue == 0 && mValues[mValues.length - 1] == mMaxValue)
+		if (mCounter2 == 0)
+		{
+			mCounter2++;
+			mTemp = aValue;
+			mValues[0] = aValue;
+		}
+		else if (mCounter2 < 9)
+		{
+			mCounter2++;
+			mTemp += aValue;
+			mValues[0] = mTemp / mCounter2;
+		}
+		else
+		{
+			mValues[0] = (mTemp + aValue) / mCounter2;
+			mCounter2 = 0;
+
+			mCumulativeSum -= mValues[mValues.length - 1];
+			mCumulativeSum += aValue;
+
+			System.arraycopy(mValues, 0, mValues, 1, mValues.length - 1);
+			System.arraycopy(mTimes, 0, mTimes, 1, mTimes.length - 1);
+			mValues[0] = aValue;
+			mTimes[0] = System.currentTimeMillis();
+
+			mCounter++;
+		}
+
+		if (mConstantMaxValue == 0)
 		{
 			mMaxValue = 0;
 			for (int i = 0; i < mValues.length - 1; i++)
 			{
 				mMaxValue = Math.max(mMaxValue, mValues[i]);
 			}
-		}
-
-		mCumulativeSum -= mValues[mValues.length - 1];
-		mCumulativeSum += aValue;
-
-		System.arraycopy(mValues, 0, mValues, 1, mValues.length - 1);
-		System.arraycopy(mTimes, 0, mTimes, 1, mTimes.length - 1);
-		mValues[0] = aValue;
-		mTimes[0] = System.currentTimeMillis();
-
-		mCounter++;
-
-		if (mConstantMaxValue == 0)
-		{
-			mMaxValue = Math.max(mMaxValue, aValue);
 		}
 
 		SwingUtilities.invokeLater(() -> repaint());
@@ -123,7 +333,7 @@ public class ProgressiveGraph extends JComponent
 		g.setColor(getBackground());
 		g.fillRect(0, 0, w, h);
 
-		TextBox textBox = new TextBox().setForeground(getForeground()).setBounds(x, 0, w1, y);
+		TextBox textBox = new TextBox().setFont(mFont).setForeground(getForeground()).setBounds(x, 0, w1, y);
 		textBox.setAnchor(Anchor.WEST).setText(mTitle).render(g);
 		textBox.setAnchor(Anchor.EAST).setText(mMaxValue + " " + mUnit).render(g);
 
@@ -133,6 +343,9 @@ public class ProgressiveGraph extends JComponent
 		drawValues(g, w1, h1);
 		drawGraph(g, w1, h1);
 
+		g.setColor(mBorderColor);
+		g.drawRect(0, 0, w1, h1);
+		
 		g.translate(x, -y);
 	}
 
@@ -141,11 +354,14 @@ public class ProgressiveGraph extends JComponent
 	{
 		double stepSize = aWidth / (double)(mValues.length - 1);
 
-		TextBox textBox = new TextBox().setForeground(getForeground()).setBounds(0, 0, aWidth, mTitleLegendHeight);
-
 		int vy = aHeight - (int)(aHeight * mValues[0] / (mMaxValue == 0 ? 1 : mMaxValue));
 
-		textBox.setBounds(aWidth + 5, vy - 9, mRightLegendWidth, 18).setAnchor(Anchor.WEST).setText("" + mValues[0]).render(g);
+		TextBox textBox = new TextBox()
+			.setFont(mFont)
+			.setForeground(getForeground())
+			.setBounds(aWidth + 5, vy - 9, mRightLegendWidth, 18)
+			.setAnchor(Anchor.WEST)
+			.setText("" + mValues[0]).render(g);
 
 		int modulo = (int)Math.max(Math.ceil(mTimeSpacing / (aWidth / (double)mValues.length)), 2);
 
@@ -243,13 +459,17 @@ public class ProgressiveGraph extends JComponent
 	{
 		try
 		{
-			ProgressiveGraph graph = new ProgressiveGraph("test", "rpm", 1000);
-			graph.setBackground(new Color(240, 240, 240));
-			graph.setGridColor(new Color(221, 226, 230));
+			ProgressiveGraph graph = new ProgressiveGraph("test", "rpm", 100);
+			graph.setBackground(Color.BLACK);
+			graph.setGridColor(new Color(32,32,32));
+			graph.setBorderColor(new Color(64,64,64));
+			graph.setFillColor(new Color(17, 187, 55, 200));
+			graph.setLineColor(new Color(17, 187, 55));
+			graph.scaleUI(2);
 
 			JFrame frame = new JFrame();
 			frame.add(graph);
-			frame.setSize(1200, 120);
+			frame.setSize(1200, 600);
 			frame.setLocationRelativeTo(null);
 			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 			frame.setVisible(true);
