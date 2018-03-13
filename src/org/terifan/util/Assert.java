1d7a142aaa077d43e1e0b8242d24b31eb935a5ad
terifan <pinjata69@gmail.com>
2018-03-13 16:18:56 +0100

    update

--- src/org/terifan/util/Assert.java
+++ src/org/terifan/util/Assert.java
@@ -27,16 +27,16 @@ public class Assert
 	/**
 	 * Throws exception if value is false.
 	 */
-	public static void verify(boolean aValue)
+	public static void assertTrue(boolean aValue)
 	{
-		verify(aValue, "Value not verified");
+		assertTrue(aValue, "Value not verified");
 	}
 
 
 	/**
 	 * Throws exception if value is false.
 	 */
-	public static void verify(boolean aValue, String aErrorMessage, Object... aArguments)
+	public static void assertTrue(boolean aValue, String aErrorMessage, Object... aArguments)
 	{
 		if (!aValue)
 		{
@@ -46,18 +46,39 @@ public class Assert
 
 
 	/**
-	 * Throws exception if value is null.
+	 * Throws exception if value is false.
 	 */
-	public static void notNull(Object aValue)
+	public static void assertEquals(Object aValue0, Object aValue1)
 	{
-		notNull(aValue, "Value is null");
+		assertEquals(aValue0, aValue1, "Values aren't equal");
+	}
+
+
+	/**
+	 * Throws exception if value is false.
+	 */
+	public static void assertEquals(Object aValue0, Object aValue1, String aErrorMessage, Object... aArguments)
+	{
+		if ((aValue0 == null ^ aValue1 == null) || aValue0 != null && !aValue0.equals(aValue1))
+		{
+			throw new IllegalArgumentException(String.format(aErrorMessage, aArguments));
+		}
 	}
 
 
 	/**
 	 * Throws exception if value is null.
 	 */
-	public static void notNull(Object aValue, String aErrorMessage, Object... aArguments)
+	public static void assertNotNull(Object aValue)
+	{
+		assertNotNull(aValue, "Value is null");
+	}
+
+
+	/**
+	 * Throws exception if value is null.
+	 */
+	public static void assertNotNull(Object aValue, String aErrorMessage, Object... aArguments)
 	{
 		if (aValue == null)
 		{
@@ -69,16 +90,16 @@ public class Assert
 	/**
 	 * Throws exception if value is empty or null.
 	 */
-	public static void notEmptyOrNull(Object aValue)
+	public static void assertNotEmptyOrNull(Object aValue)
 	{
-		notEmptyOrNull(aValue, "Value is empty or null");
+		assertNotEmptyOrNull(aValue, "Value is empty or null");
 	}
 
 
 	/**
 	 * Throws exception if value is empty or null.
 	 */
-	public static void notEmptyOrNull(Object aValue, String aErrorMessage, Object... aArguments)
+	public static void assertNotEmptyOrNull(Object aValue, String aErrorMessage, Object... aArguments)
 	{
 		if (aValue == null || aValue.toString().isEmpty())
 		{
@@ -90,16 +111,37 @@ public class Assert
 	/**
 	 * Throws exception if value don't match regular expression.
 	 */
-	public static void matches(Object aValue, String aPattern)
+	public static void assertMatchesOrNull(Object aValue, String aPattern)
 	{
-		matches(aValue, aPattern, "Value don't match pattern");
+		assertMatchesOrNull(aValue, aPattern, "Value don't match pattern");
 	}
 
 
 	/**
 	 * Throws exception if value don't match regular expression.
 	 */
-	public static void matches(Object aValue, String aPattern, String aErrorMessage, Object... aArguments)
+	public static void assertMatchesOrNull(Object aValue, String aPattern, String aErrorMessage, Object... aArguments)
+	{
+		if (aValue != null && !aValue.toString().matches(aPattern))
+		{
+			throw new IllegalArgumentException(String.format(aErrorMessage, aArguments));
+		}
+	}
+
+
+	/**
+	 * Throws exception if value don't match regular expression.
+	 */
+	public static void assertMatches(Object aValue, String aPattern)
+	{
+		assertMatches(aValue, aPattern, "Value don't match pattern");
+	}
+
+
+	/**
+	 * Throws exception if value don't match regular expression.
+	 */
+	public static void assertMatches(Object aValue, String aPattern, String aErrorMessage, Object... aArguments)
 	{
 		if (aValue == null || !aValue.toString().matches(aPattern))
 		{
