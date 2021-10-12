3885a756059fffe7a1a4036cc47777f0fa9d8b62
terifan <pinjata69@gmail.com>
2021-10-12 10:42:28 +0200

    update

--- src/org/terifan/ui/StyleSet.java
+++ src/org/terifan/ui/StyleSet.java
@@ -1,20 +1,22 @@
 package org.terifan.ui;
 
 import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Image;
+import java.awt.Insets;
 import java.util.HashMap;
+import java.util.Map;
 
 
 public class StyleSet
 {
 	private HashMap<String, Color> mColors = new HashMap<>();
-
-//	private HashMap<Integer, Color> mColors = new HashMap<>();
-//
-//	public final static int UNFOCUSED = 0;
-//	public final static int FOCUSED = 1;
-//	public final static int ARMED = 2;
-//	public final static int PRESSED = 4;
-//	public final static int FOCUSEDPARENT = 8;
+	private HashMap<String, Font> mFonts = new HashMap<>();
+	private HashMap<String, Insets> mInsets = new HashMap<>();
+	private HashMap<String, Integer> mIntegers = new HashMap<>();
+	private HashMap<String, Image> mImages = new HashMap<>();
+	private HashMap<String, Dimension> mDimensions = new HashMap<>();
 
 
 	public StyleSet()
@@ -22,94 +24,165 @@ public class StyleSet
 	}
 
 
-	public StyleSet add(String aFilter, Color aColor)
+	public StyleSet set(String aFilter, Color aColor)
 	{
+		put(mColors, aFilter, aColor);
 		return this;
 	}
 
 
-	public Color getColor(String aName, State aState)
+	public StyleSet set(String aFilter, Font aFont)
 	{
-		return mColors.get(aName);
+		put(mFonts, aFilter, aFont);
+		return this;
 	}
 
 
-//	public StyleSet add(Color aColor, int... aId)
-//	{
-//		for (int i : aId)
-//		{
-//			mColors.put(i, aColor);
-//		}
-//		return this;
-//	}
-//
-//
-//	public StyleSet add(boolean aFocused, Color aColor)
-//	{
-//		mColors.put(id(aFocused, false, false), aColor);
-//		return this;
-//	}
-//
-//
-//	public StyleSet add(boolean aFocused, boolean aArmed, Color aColor)
-//	{
-//		mColors.put(id(aFocused, aArmed, false), aColor);
-//		return this;
-//	}
-//
-//
-//	public StyleSet add(boolean aFocused, boolean aArmed, boolean aPressed, Color aColor)
-//	{
-//		mColors.put(id(aFocused, aArmed, aPressed), aColor);
-//		return this;
-//	}
-//
-//
-//	public Color get(int aId)
-//	{
-//		return get((aId & FOCUSED) != 0, (aId & ARMED) != 0, (aId & PRESSED) != 0);
-//	}
-//
-//
-//	public Color get(boolean aFocused)
-//	{
-//		return get(aFocused, false, false);
-//	}
-//
-//
-//	public Color get(boolean aFocused, boolean aArmed)
-//	{
-//		return get(aFocused, aArmed, false);
-//	}
-//
-//
-//	public Color get(boolean aFocused, boolean aArmed, boolean aPressed)
-//	{
-//		Color c = mColors.get(id(aFocused, aArmed, aPressed));
-//		if (c == null)
-//		{
-//			c = mColors.get(id(aFocused, aArmed, false));
-//		}
-//		if (c == null)
-//		{
-//			c = mColors.get(id(aFocused, false, false));
-//		}
-//		if (c == null)
-//		{
-//			c = mColors.get(id(false, false, false));
-//		}
-//		if (c == null)
-//		{
-//			c = Color.RED;
-//		}
-//		return c;
-//	}
-//
-//
-//	protected int id(boolean aFocused, boolean aArmed, boolean aPressed)
-//	{
-//		return (aFocused ? FOCUSED : 0) + (aArmed ? ARMED : 0) + (aPressed ? PRESSED : 0);
-//	}
+	public StyleSet set(String aFilter, int aInteger)
+	{
+		put(mIntegers, aFilter, aInteger);
+		return this;
+	}
+
+
+	public StyleSet set(String aFilter, Insets aInsets)
+	{
+		put(mInsets, aFilter, aInsets);
+		return this;
+	}
+
+
+	public StyleSet set(String aFilter, Image aImage)
+	{
+		put(mImages, aFilter, aImage);
+		return this;
+	}
+
+
+	public StyleSet set(String aFilter, Dimension aDimension)
+	{
+		put(mDimensions, aFilter, aDimension);
+		return this;
+	}
+
+
+	public Color getColor(String aName)
+	{
+		return getColor(aName, false, false, false, true);
+	}
+
+
+	public Color getColor(String aName, boolean aArmed, boolean aHover)
+	{
+		return getColor(aName, aArmed, aHover, false, true);
+	}
+
+
+	public Color getColor(String aName, boolean aArmed, boolean aHover, boolean aSelected)
+	{
+		return getColor(aName, aArmed, aHover, aSelected, true);
+	}
+
+
+	public Color getColor(String aName, boolean aArmed, boolean aHover, boolean aSelected, boolean aFocusedWindow)
+	{
+		return mColors.get(find(mColors, aName, aArmed, aHover, aSelected, aFocusedWindow));
+	}
+
+
+	public int getInt(String aName)
+	{
+		return mIntegers.get(find(mIntegers, aName, false, false, false, false));
+	}
+
+
+	public Dimension getDimension(String aName)
+	{
+		return mDimensions.get(find(mDimensions, aName, false, false, false, false));
+	}
+
+
+	public Insets getInsets(String aName)
+	{
+		return mInsets.get(find(mInsets, aName, false, false, false, false));
+	}
+
+
+	public Font getFont(String aName)
+	{
+		return mFonts.get(find(mFonts, aName, false, false, false, false));
+	}
+
+
+	public Image getImage(String aName)
+	{
+		return mImages.get(find(mImages, aName, false, false, false, false));
+	}
+
+
+	private String find(Map<String, ?> aMap, String aKey, boolean aArmed, boolean aHover, boolean aSelected, boolean aFocusedWindow)
+	{
+		String type = toTypeString(aSelected, aArmed, aHover, aFocusedWindow);
+
+		String bestKey = null;
+		String bestMatches = "";
+
+		for (String key : aMap.keySet())
+		{
+			String[] args = key.split(":");
+			if (aKey.matches(args[0]))
+			{
+				if (args.length == 1)
+				{
+					if (bestMatches.isEmpty())
+					{
+						bestKey = key;
+					}
+				}
+				else
+				{
+					for (String m : args[1].split("\\|"))
+					{
+						if (type.equals(toTypeString(m.contains("s"), m.contains("a"), m.contains("h"), !m.contains("u"))))
+						{
+							bestKey = key;
+							bestMatches = m;
+						}
+					}
+				}
+			}
+		}
+
+		return bestKey;
+	}
+
+
+	public String toTypeString(boolean aSelected, boolean aArmed, boolean aHover, boolean aFocusedWindow)
+	{
+		return (aSelected ? "s" : "-") + (aArmed ? "a" : "-") + (aHover ? "h" : "-") + (aFocusedWindow ? "f" : "-");
+	}
+
+
+	private <T> void put(HashMap<String, T> aMap, String aFilter, T aValue)
+	{
+		String[] args = aFilter.split(":");
+
+		for (String key : args[0].split("\\|"))
+		{
+			if (args.length > 1)
+			{
+				key += ":" + args[1];
+			}
+			if (aValue == null)
+			{
+				aMap.remove(key);
+			}
+			else
+			{
+				aMap.put(key, aValue);
+			}
+		}
+	}
 
 
 	public static class State
@@ -122,16 +195,16 @@ public class StyleSet
 	}
 
 
-	public static void main(String ... args)
+	public static void main(String... args)
 	{
 		try
 		{
 			StyleSet styles = new StyleSet();
-			styles.add("MenuForegroundShadow:uf,s;h,a;a", new Color(35, 35, 35));
+			styles.set("MenuForegroundShadow:uf,s;h,a;a", new Color(35, 35, 35));
 
 			State state = new State();
 
-			styles.getColor("MenuForegroundShadow", state);
+//			styles.getColor("MenuForegroundShadow", state);
 		}
 		catch (Throwable e)
 		{
