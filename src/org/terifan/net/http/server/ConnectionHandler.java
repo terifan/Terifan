68a29d73eb3368b987b6bbc0879ce326d0e6250b
terifan <pinjata69@gmail.com>
2021-12-21 16:43:43 +0100

    update

--- src/org/terifan/net/http/server/ConnectionHandler.java
+++ src/org/terifan/net/http/server/ConnectionHandler.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
-import java.text.SimpleDateFormat;
 import java.util.LinkedHashMap;
 import org.terifan.io.ByteArray;
 import org.terifan.util.log.Log;
