0d936791486c55e8887418394e944a61dcd03245
terifan <pinjata69@gmail.com>
2023-05-02 13:55:53 +0200

    update

--- src/org/terifan/util/AsyncTask.java
+++ src/org/terifan/util/AsyncTask.java
@@ -1,9 +1,19 @@
 package org.terifan.util;
 
+import java.util.concurrent.TimeUnit;
+import org.terifan.util.executors.FixedThreadExecutor;
 
-public abstract class AsyncTask<Result, Progress>
+
+public abstract class AsyncTask<Param, Progress, Result>
 {
+	private static final Object LOCK = new Object();
+
+	private static FixedThreadExecutor mExecutor;
+	private static Object mParallelism = 1;
+
 	private boolean mCancelled;
+	private boolean mFinished;
+	private Result mResult;
 
 
 	protected AsyncTask()
@@ -12,86 +22,106 @@ public abstract class AsyncTask<Result, Progress>
 
 
 	/**
-	 * Call this to start the AsyncTask. This method invokes onPreExecute, doInBackground, onPostExecute in that order.
+	 * Sets how many concurrent threads will process tasks. This value is used next time an executor is created which occur when all
+	 * pending tasks have been finished.
+	 *
+	 * @param aParallelism a positive number equals number of threads to use, zero or a negative number results in total available processors
 	 */
-	public final AsyncTask execute()
+	public static void setParallelism(int aParallelism)
 	{
-		new Thread()
-		{
-			@Override
-			public void run()
-			{
-				try
-				{
-					onPreExecute();
-
-					Result result = doInBackground();
-
-					if (mCancelled)
-					{
-						onCancelled(result);
-					}
-					else
-					{
-						onPostExecute(result);
-					}
-				}
-				catch (Throwable e)
-				{
-					try
-					{
-						onError(e);
-					}
-					catch (Throwable ee)
-					{
-						// ignore
-					}
-				}
-			}
-		}.start();
-
-		return this;
+		mParallelism = aParallelism;
 	}
 
 
 	/**
-	 * Convenience method to invoke a Runnable. Methods onCancelled and onPostExecute are called with a <code>null</code> as parameter.
+	 * Sets how many concurrent threads will process tasks. This value is used next time an executor is created which occur when all
+	 * pending tasks have been finished.
+	 *
+	 * @param aParallelism number of threads expressed as a number between 0 and 1 out of total available CPUs
 	 */
-	public final AsyncTask execute(Runnable aRunnable)
+	public static void setParallelism(float aParallelism)
 	{
-		new Thread()
+		mParallelism = aParallelism;
+	}
+
+
+	/**
+	 * Convenience version of execute(java.lang.Object) for use with a simple Runnable object.
+	 */
+	public static void execute(Runnable aRunnable)
+	{
+		AsyncTask task = new AsyncTask()
 		{
 			@Override
-			public void run()
+			protected Object doInBackground(Object aParam) throws Throwable
+			{
+				aRunnable.run();
+				return null;
+			}
+		};
+		task.execute("");
+	}
+
+
+	/**
+	 * Add this AsyncTask to the internal execution queue. When the AsyncTask is executed, methods onPreExecute, doInBackground,
+	 * onPostExecute are called in that order.
+	 */
+	public final AsyncTask execute(Param aParam)
+	{
+		Runnable task = () ->
+		{
+			try
+			{
+				onPreExecute();
+
+				Result result = doInBackground(aParam);
+
+				if (mCancelled)
+				{
+					onCancelled(result);
+				}
+				else
+				{
+					onPostExecute(result);
+				}
+			}
+			catch (Throwable e)
 			{
 				try
 				{
-					onPreExecute();
-
-					aRunnable.run();
-
-					if (mCancelled)
-					{
-						onCancelled(null);
-					}
-					else
-					{
-						onPostExecute(null);
-					}
+					onError(e);
 				}
-				catch (Throwable e)
+				catch (Throwable ee)
 				{
-					try
-					{
-						onError(e);
-					}
-					catch (Throwable ee)
-					{
-						// ignore
-					}
+					// ignore
 				}
 			}
-		}.start();
+			finally
+			{
+				synchronized (LOCK)
+				{
+					mFinished = true;
+					LOCK.notify();
+				}
+			}
+		};
+
+		synchronized (LOCK)
+		{
+			if (mExecutor == null)
+			{
+				if (mParallelism instanceof Float)
+				{
+					mExecutor = new FixedThreadExecutor((Float)mParallelism);
+				}
+				else
+				{
+					mExecutor = new FixedThreadExecutor((Integer)mParallelism);
+				}
+			}
+			mExecutor.submit(task::run);
+		}
 
 		return this;
 	}
@@ -100,10 +130,9 @@ public abstract class AsyncTask<Result, Progress>
 	/**
 	 * Implementation of the AsyncTask.
 	 *
-	 * @return
-	 *   this value will be forwarded to either <code>onPostExecute</code> or <code>onCancelled</code>.
+	 * @return this value will be forwarded to either <code>onPostExecute</code> or <code>onCancelled</code>.
 	 */
-	protected abstract Result doInBackground() throws Throwable;
+	protected abstract Result doInBackground(Param aParam) throws Throwable;
 
 
 	/**
@@ -123,7 +152,7 @@ public abstract class AsyncTask<Result, Progress>
 
 
 	/**
-	 * This method can be invoked from <code>doInBackground</code> to report progress.
+	 * This is an optional method that can be invoked from <code>doInBackground</code> to report progress.
 	 */
 	protected void onProgressUpdate(Progress aProgress)
 	{
@@ -131,7 +160,7 @@ public abstract class AsyncTask<Result, Progress>
 
 
 	/**
-	 * This method can be invoked from <code>doInBackground</code> to update the result.
+	 * This is an optional method that can be invoked from <code>doInBackground</code> to update the result.
 	 */
 	protected void onResultUpdate(Result aResult)
 	{
@@ -170,4 +199,156 @@ public abstract class AsyncTask<Result, Progress>
 	{
 		mCancelled = true;
 	}
+
+
+	/**
+	 * Retrieves the result or null if the computation isn't finished.
+	 */
+	public Result peek()
+	{
+		return mResult;
+	}
+
+
+	/**
+	 * Waits if necessary for the computation to complete, and then retrieves its result.
+	 */
+	public Result get()
+	{
+		return get(Long.MAX_VALUE, TimeUnit.MILLISECONDS);
+	}
+
+
+	/**
+	 * Waits if necessary for at most the given time for the computation to complete, and then retrieves its result.
+	 */
+	public Result get(long aTimeOut, TimeUnit aTimeUnit)
+	{
+		long millis = aTimeUnit.toMillis(aTimeOut);
+
+		for (long startTime = System.currentTimeMillis(); millis > System.currentTimeMillis() - startTime;)
+		{
+			synchronized (LOCK)
+			{
+				if (mFinished)
+				{
+					return mResult;
+				}
+				try
+				{
+					LOCK.wait(1000);
+				}
+				catch (InterruptedException ex)
+				{
+				}
+			}
+		}
+		return mResult;
+	}
+
+
+	/**
+	 * Waits if necessary until the currently queued tasks have been finished. Other threads can still enqueue new tasks.
+	 */
+	public static void waitFor()
+	{
+		FixedThreadExecutor prev = mExecutor;
+		mExecutor = null;
+		if (prev != null)
+		{
+			prev.close();
+		}
+	}
+
+
+//	public static void main(String... args)
+//	{
+//		try
+//		{
+//			for (int i = 0; i < 1000; i++)
+//			{
+//				AsyncTask<Float, Integer, String> task = new AsyncTask<Float, Integer, String>()
+//				{
+//					@Override
+//					protected String doInBackground(Float aParam) throws Throwable
+//					{
+//						for (int i = 0; i <= 10; i++)
+//						{
+//							onProgressUpdate(10 * i);
+//						}
+//						return "value=" + aParam;
+//					}
+//
+//
+//					@Override
+//					protected void onPostExecute(String aResult) throws Throwable
+//					{
+//						System.out.println(aResult);
+//					}
+//
+//
+//					@Override
+//					protected void onProgressUpdate(Integer aProgress)
+//					{
+//						System.out.println(aProgress + "%");
+//					}
+//				};
+//
+//				task.execute((float)i);
+//			}
+//
+//			AsyncTask.execute(() -> System.out.println("#"));
+//
+//			System.out.println("#".repeat(100));
+//
+//			AsyncTask.waitFor();
+//			AsyncTask.setParallelism(4);
+//
+//			System.out.println("!".repeat(100));
+//
+//			for (int i = 0; i < 1000; i++)
+//			{
+//				AsyncTask<Float, Integer, String> task = new AsyncTask<Float, Integer, String>()
+//				{
+//					@Override
+//					protected String doInBackground(Float aParam) throws Throwable
+//					{
+//						for (int i = 0; i <= 10; i++)
+//						{
+//							onProgressUpdate(10 * i);
+//						}
+//						return "value=" + aParam;
+//					}
+//
+//
+//					@Override
+//					protected void onPostExecute(String aResult) throws Throwable
+//					{
+//						System.out.println(aResult);
+//					}
+//
+//
+//					@Override
+//					protected void onProgressUpdate(Integer aProgress)
+//					{
+//						System.out.println(aProgress + "%");
+//					}
+//				};
+//
+//				task.execute((float)i);
+//			}
+//
+//			AsyncTask.execute(() -> System.out.println("#"));
+//
+//			System.out.println("#".repeat(100));
+//
+//			AsyncTask.finish();
+//
+//			System.out.println("!".repeat(100));
+//		}
+//		catch (Throwable e)
+//		{
+//			e.printStackTrace(System.out);
+//		}
+//	}
 }
