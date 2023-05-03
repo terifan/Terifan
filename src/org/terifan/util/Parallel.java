8cea4e12300702e84d8515cdccf0e57edcab3177
terifan <pinjata69@gmail.com>
2023-05-03 17:31:25 +0200

    update

--- src/org/terifan/util/Parallel.java
+++ src/org/terifan/util/Parallel.java
@@ -11,20 +11,6 @@ import java.util.function.Consumer;
 
 public class Parallel
 {
-//	private Parallel(int aFrom, int aTo, int aStep, boolean aInclusive)
-//	{
-//		if (aStep <= 0)
-//		{
-//			throw new IllegalArgumentException("Step must be positive.");
-//		}
-//
-//		mFrom = aFrom;
-//		mTo = aTo;
-//		mStep = aStep;
-//		mInclusive = aInclusive;
-//	}
-
-
 	private Parallel()
 	{
 	}
@@ -83,7 +69,7 @@ public class Parallel
 	}
 
 
-	static class ParallelImpl
+	private static class ParallelImpl
 	{
 		final int mFrom;
 		final int mTo;
@@ -105,7 +91,7 @@ public class Parallel
 	}
 
 
-	static class ParallelSingle extends ParallelImpl
+	public static class ParallelSingle extends ParallelImpl
 	{
 		ParallelSingle(int aFrom, int aTo, boolean aInclusive)
 		{
@@ -163,7 +149,7 @@ public class Parallel
 	}
 
 
-	static class ParallelStep extends ParallelImpl
+	public static class ParallelStep extends ParallelImpl
 	{
 		ParallelStep(int aFrom, int aTo, int aStep, boolean aInclusive)
 		{
@@ -210,38 +196,38 @@ public class Parallel
 	}
 
 
-	public static void main(String... args)
-	{
-		try
-		{
-			Parallel.of(
-				()->{System.out.println(1);},
-				()->{System.out.println(2);},
-				()->{System.out.println(3);},
-				()->{System.out.println(4);},
-				()->{System.out.println(5);},
-				()->{System.out.println(6);},
-				()->{System.out.println(7);},
-				()->{System.out.println(8);},
-				()->{System.out.println(9);}
-			);
-
-			Parallel.range(0, 10, 4).forEach((i,j) -> System.out.println(i));
-			System.out.println("-----");
-			Parallel.rangeClosed(0, 10, 5).forEach((i,j) -> System.out.println(i));
-			System.out.println("-----");
-			Parallel.range(0, 3).forEach((i) -> System.out.println(i));
-			System.out.println("-----");
-			Parallel.rangeClosed(0, 3).forEach((i) -> System.out.println(i));
-			System.out.println("-----");
-			Parallel.range(0, 10, 4).forEach((i, j) -> System.out.println(i + ", " + j));
-			System.out.println("-----");
-			Parallel.rangeClosed(0, 10, 4).forEach((i, j) -> System.out.println(i + ", " + j));
-			System.out.println("-----");
-		}
-		catch (Throwable e)
-		{
-			e.printStackTrace(System.out);
-		}
-	}
+//	public static void main(String... args)
+//	{
+//		try
+//		{
+//			Parallel.of(
+//				()->{System.out.println(1);},
+//				()->{System.out.println(2);},
+//				()->{System.out.println(3);},
+//				()->{System.out.println(4);},
+//				()->{System.out.println(5);},
+//				()->{System.out.println(6);},
+//				()->{System.out.println(7);},
+//				()->{System.out.println(8);},
+//				()->{System.out.println(9);}
+//			);
+//
+//			Parallel.range(0, 10, 4).forEach((i,j) -> System.out.println(i));
+//			System.out.println("-----");
+//			Parallel.rangeClosed(0, 10, 5).forEach((i,j) -> System.out.println(i));
+//			System.out.println("-----");
+//			Parallel.range(0, 3).forEach((i) -> System.out.println(i));
+//			System.out.println("-----");
+//			Parallel.rangeClosed(0, 3).forEach((i) -> System.out.println(i));
+//			System.out.println("-----");
+//			Parallel.range(0, 10, 4).forEach((i, j) -> System.out.println(i + ", " + j));
+//			System.out.println("-----");
+//			Parallel.rangeClosed(0, 10, 4).forEach((i, j) -> System.out.println(i + ", " + j));
+//			System.out.println("-----");
+//		}
+//		catch (Throwable e)
+//		{
+//			e.printStackTrace(System.out);
+//		}
+//	}
 }
