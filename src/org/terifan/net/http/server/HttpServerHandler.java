ea90c7d1c2471338861f1ce17c26debc0bb0dc5b
terifan <pinjata69@gmail.com>
2024-03-18 17:09:50 +0100

    update

--- src/org/terifan/net/http/server/HttpServerHandler.java
+++
@@ -1,9 +0,0 @@
-package org.terifan.net.http.server;
-
-import java.io.IOException;
-
-
-public interface HttpServerHandler
-{
-	void service(HttpServerRequest aRequest, HttpServerResponse aResponse) throws IOException;
-}
