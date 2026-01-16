bc1063fa164b07a2fea4252629853b83eec42abe
terifan <pinjata69@gmail.com>
2026-01-16 13:55:04 +0100

    update

--- src/org/terifan/util/Constant.java
+++ src/org/terifan/util/Constant.java
@@ -1,13 +1,35 @@
 package org.terifan.util;
 
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.ListIterator;
+import java.util.Map;
+import java.util.Set;
 import java.util.function.Function;
 import java.util.function.Supplier;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 
+/**
+ * Warning: implementation not tested....
+ *
+ * e.g.
+ * <pre>
+ * class Person
+ * {
+ *    private final Constant<String> mCountry = Constant.of();
+ *
+ *    public String getCountry()
+ *    {
+ *       return mCountry.orElseSet(() -> "Sweden"); // expensive operation to find the country
+ *    }
+ *	}
+ * </pre>
+ *
+ */
 public class Constant<T>
 {
 	private volatile T mInstance;
@@ -22,7 +44,14 @@ public class Constant<T>
 	public static <T> Constant<T> of()
 	{
 		Constant c = new Constant();
-//		c.mInstance = aValue;
+		return c;
+	}
+
+
+	public static <T> Constant<T> of(T aValue)
+	{
+		Constant c = new Constant();
+		c.mInstance = aValue;
 		return c;
 	}
 
@@ -35,13 +64,128 @@ public class Constant<T>
 	}
 
 
-	public static <T,U> List<T> list(int aSize, Function<Object,T> aProducer)
+	public static <T, U> Function<T, U> function(Set<T> aKeys, Function<T, U> aFunction)
 	{
+		HashMap<T, U> map = new HashMap<>();
+		return aKey ->
+		{
+			if (!aKeys.contains(aKey))
+			{
+				throw new IllegalArgumentException("" + aKey);
+			}
+			return map.computeIfAbsent(aKey, k -> aFunction.apply(k));
+		};
+	}
+
+
+	public static <T, U> Function<T, U> function(Function<T, U> aFunction)
+	{
+		HashMap<T, U> map = new HashMap<>();
+		return aKey -> map.computeIfAbsent(aKey, k -> aFunction.apply(k));
+	}
+
+
+	public static <T, U> Map<T, U> map(Set<T> aKeys, Function<T, U> aFunction)
+	{
+		HashMap<T, U> map = new HashMap<>();
+		return new Map<T, U>()
+		{
+			@Override
+			public int size()
+			{
+				return aKeys.size();
+			}
+
+
+			@Override
+			public boolean isEmpty()
+			{
+				return aKeys.isEmpty();
+			}
+
+
+			@Override
+			public boolean containsKey(Object aKey)
+			{
+				return aKeys.contains((T)aKey);
+			}
+
+
+			@Override
+			public boolean containsValue(Object aValue)
+			{
+				throw new UnsupportedOperationException();
+			}
+
+
+			@Override
+			public U get(Object aKey)
+			{
+				if (!aKeys.contains((T)aKey))
+				{
+					throw new IllegalArgumentException("" + aKey);
+				}
+				return map.computeIfAbsent((T)aKey, k -> aFunction.apply(k));
+			}
+
+
+			@Override
+			public U put(T aKey, U aValue)
+			{
+				return map.put(aKey, aValue);
+			}
+
+
+			@Override
+			public U remove(Object aKey)
+			{
+				return map.remove((T)aKey);
+			}
+
+
+			@Override
+			public void putAll(Map<? extends T, ? extends U> aMap)
+			{
+				map.putAll(aMap);
+			}
+
+
+			@Override
+			public void clear()
+			{
+				map.clear();
+			}
+
+
+			@Override
+			public Set<T> keySet()
+			{
+				return aKeys;
+			}
+
+
+			@Override
+			public Collection<U> values()
+			{
+				throw new UnsupportedOperationException();
+			}
+
+
+			@Override
+			public Set<Map.Entry<T, U>> entrySet()
+			{
+				throw new UnsupportedOperationException();
+			}
+		};
+	}
+
+
+	public static <T> List<T> list(int aSize, Function<Object, T> aProducer)
+	{
+		HashMap<Integer, T> map = new HashMap<>();
+
 		return new List<T>()
 		{
-			Object[] buffer = new Object[aSize];
-
-
 			@Override
 			public int size()
 			{
@@ -52,106 +196,122 @@ public class Constant<T>
 			@Override
 			public boolean isEmpty()
 			{
-				throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+				return aSize == 0;
 			}
 
 
 			@Override
-			public boolean contains(Object aO)
+			public boolean contains(Object aObject)
 			{
-				throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+				throw new UnsupportedOperationException();
 			}
 
 
 			@Override
 			public Iterator<T> iterator()
 			{
-				throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+				return new Iterator<T>()
+				{
+					int i;
+
+					@Override
+					public boolean hasNext()
+					{
+						return i < aSize;
+					}
+
+
+					@Override
+					public T next()
+					{
+						return get(i++);
+					}
+				};
 			}
 
 
 			@Override
 			public Object[] toArray()
 			{
-				throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+				return StreamSupport.stream(((Iterable<T>)() -> iterator()).spliterator(), false).collect(Collectors.toList()).toArray();
 			}
 
 
 			@Override
 			public <T> T[] toArray(T[] a)
 			{
-				throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+				return StreamSupport.stream(((Iterable<T>)() -> (Iterator<T>)iterator()).spliterator(), false).collect(Collectors.toList()).toArray(a);
 			}
 
 
 			@Override
 			public boolean add(T aE)
 			{
-				throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+				throw new UnsupportedOperationException();
 			}
 
 
 			@Override
 			public boolean remove(Object aO)
 			{
-				throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+				throw new UnsupportedOperationException();
 			}
 
 
 			@Override
 			public boolean containsAll(Collection<?> aC)
 			{
-				throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+				throw new UnsupportedOperationException();
 			}
 
 
 			@Override
 			public boolean addAll(Collection<? extends T> aC)
 			{
-				throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+				throw new UnsupportedOperationException();
 			}
 
 
 			@Override
 			public boolean addAll(int aIndex, Collection<? extends T> aC)
 			{
-				throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+				throw new UnsupportedOperationException();
 			}
 
 
 			@Override
 			public boolean removeAll(Collection<?> aC)
 			{
-				throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+				throw new UnsupportedOperationException();
 			}
 
 
 			@Override
 			public boolean retainAll(Collection<?> aC)
 			{
-				throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+				throw new UnsupportedOperationException();
 			}
 
 
 			@Override
 			public void clear()
 			{
-				throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+				throw new UnsupportedOperationException();
 			}
 
 
 			@Override
 			public T get(int aIndex)
 			{
-				T v = (T)buffer[aIndex];
+				T v = (T)map.get(aIndex);
 				if (v == null)
 				{
 					synchronized (this)
 					{
-						v = (T)buffer[aIndex];
+						v = (T)map.get(aIndex);
 						if (v == null)
 						{
-							buffer[aIndex] = v = aProducer.apply(aIndex);
+							map.put(aIndex, v = aProducer.apply(aIndex));
 						}
 					}
 				}
@@ -162,58 +322,76 @@ public class Constant<T>
 			@Override
 			public T set(int aIndex, T aElement)
 			{
-				throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+				return map.put(aIndex, aElement);
 			}
 
 
 			@Override
 			public void add(int aIndex, T aElement)
 			{
-				throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+				throw new UnsupportedOperationException();
 			}
 
 
 			@Override
 			public T remove(int aIndex)
 			{
-				throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+				throw new UnsupportedOperationException();
 			}
 
 
 			@Override
 			public int indexOf(Object aO)
 			{
-				throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+				throw new UnsupportedOperationException();
 			}
 
 
 			@Override
 			public int lastIndexOf(Object aO)
 			{
-				throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+				throw new UnsupportedOperationException();
 			}
 
 
 			@Override
 			public ListIterator<T> listIterator()
 			{
-				throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+				throw new UnsupportedOperationException();
 			}
 
 
 			@Override
 			public ListIterator<T> listIterator(int aIndex)
 			{
-				throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+				throw new UnsupportedOperationException();
 			}
 
 
 			@Override
 			public List<T> subList(int aFromIndex, int aToIndex)
 			{
-				throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+				throw new UnsupportedOperationException();
 			}
 		};
+	}
+
+
+	public T orElse(T aOther)
+	{
+		T v = mInstance;
+		if (v == null)
+		{
+			synchronized (this)
+			{
+				v = mInstance;
+				if (v == null)
+				{
+					mInstance = v = aOther;
+				}
+			}
+		}
+		return v;
 	}
 
 
@@ -228,6 +406,24 @@ public class Constant<T>
 				if (v == null)
 				{
 					mInstance = v = aSupplier.get();
+				}
+			}
+		}
+		return v;
+	}
+
+
+	public T orElseThrow()
+	{
+		T v = mInstance;
+		if (v == null)
+		{
+			synchronized (this)
+			{
+				v = mInstance;
+				if (v == null)
+				{
+					throw new IllegalStateException();
 				}
 			}
 		}
--- src/org/terifan/util/_TestConstant.java
+++ src/org/terifan/util/_TestConstant.java
@@ -1,29 +1,49 @@
 package org.terifan.util;
 
 import java.awt.Dimension;
-import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.Random;
+import java.util.Set;
 import java.util.UUID;
-import java.util.function.Supplier;
-import java.util.stream.Stream;
+import java.util.function.Function;
 
 
 public class _TestConstant
 {
 	private final static Constant<UUID> ID = Constant.supplier(() -> UUID.randomUUID());
 
-//	private final static Constant<int[]> VALS = Constant.supplier(() -> new int[]
-//	{
-//		1, 2, 3
-//	});
+	private final static Constant<int[]> VALS = Constant.supplier(() -> new int[]
+		{
+			rnd(), rnd(), rnd()
+		});
 
-	private final static Constant<int[]> VALS = Constant.of();
+	private final static List<Dimension> DIMS = Constant.list(3, x -> {System.out.println("#dim");return new Dimension((Integer)x, (Integer)x);});
 
-	private final static List<Dimension> DIMS = Constant.list(3, x -> new Dimension((Integer)x, (Integer)x));
+	private final static Function<Integer, String> numbers = Constant.function(n -> System.nanoTime() + "***" + n + " " + rnd());
 
-	private static Supplier<int[]> vals()
+	private final static Set<String> cities = Set.of("London", "Madrid", "Paris");
+
+	private final static Function<String, String> cityToCountry = Constant.function(cities, city -> System.nanoTime() + "***" + city);
+
+	private final static Map<String, String> cityToCountryMap = Constant.map(cities, city -> System.nanoTime() + "***" + city);
+
+
+	static class Person
 	{
-		return () -> new int[]{1,2,3};
+		private final static Constant<String> mCountry = Constant.of();
+
+		public static String getCountry()
+		{
+			return mCountry.orElseSet(() -> "Sweden");
+		}
+	}
+
+
+	private static int rnd()
+	{
+		System.out.println("#rnd");
+		return new Random().nextInt();
 	}
 
 
@@ -31,13 +51,28 @@ public class _TestConstant
 	{
 		try
 		{
-			UUID id = ID.get();
-			int[] i = VALS.orElseSet(vals());
-			Dimension dim = DIMS.get(2);
+//			int[] i = getValues();
+//			int[] j = getValues();
+//			int[] k = getValues();
 
-			System.out.println(id);
-			System.out.println(Arrays.toString(i));
-			System.out.println(dim);
+			System.out.println(ID.get());
+//			System.out.println(Arrays.toString(i));
+			System.out.println(DIMS.get(2));
+			System.out.println(DIMS.get(3));
+			System.out.println(cityToCountry.apply("London"));
+			System.out.println(cityToCountryMap.get("London"));
+			System.out.println(numbers.apply(7));
+			System.out.println(numbers.apply(7));
+			System.out.println(VALS.get()[0]);
+
+			System.out.println(cityToCountryMap.keySet());
+			System.out.println(cityToCountryMap.size());
+			System.out.println(""+cityToCountryMap.get("London"));
+			System.out.println(""+cityToCountry.apply("London"));
+
+			System.out.println(numbers.apply(6));
+			System.out.println(numbers.apply(7));
+			System.out.println(numbers.apply(6));
 		}
 		catch (Throwable e)
 		{
